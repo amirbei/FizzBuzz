eb8e64f7dabe599a0a96aee33043a3097be09499
Amir B <amir.beisembayev@mail.utoronto.ca>
2024-09-27 16:13:26 -0400

    added Multiples

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+
+        int steps = doReduce(n);
+
+        System.out.println(steps);
+
+    }
+
+    public static int doReduce(int n) {
+        int steps = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            steps ++;
+        }
+        return steps;
+    }
 }
+
+
